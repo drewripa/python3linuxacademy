18738d86cbb7713377e7c52a7ec70fb9c2c10024
drewripa <ripa.andrew@gmail.com>
2018-03-14 12:57:07 +0200

    Added age input example

---
+++ age.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.6
+name = input("What is your name? ")
+bday = input("What is your birthdate? ")
+age = int(input("How old are you? "))
+
+print(f"{name} was bor on {bday}")
+print(f"half of your age is {age/2}")
