9f8761577a58444c8e830a9a3dbaf438c5b8d129
drewripa <ripa.andrew@gmail.com>
2018-03-14 11:31:31 +0200

    Added 1st task

---
+++ task_0_0.py
@@ -0,0 +1,7 @@
+first_name = "Andrew"
+last_name = "Ripa"
+age = 26
+birth_date = "08/09/1991"
+
+print(f"My name is {first_name} {last_name}.")
+print(f"I was born on {birth_date}, and I'm {age} years old.")
