ea9d421f64030aa3d3e7c8f99fe1d40edf678f3d
drewripa <ripa.andrew@gmail.com>
2018-03-16 12:03:31 +0200

    Fix readlines

--- receipts/generator_t.py
+++ receipts/generator_t.py
@@ -3,7 +3,7 @@ import json
 import os
 
 count = int(os.getenv("FILE_COUNT") or 100)
-words = [word.strip() for word in open('/usr/share/dict/words').readline()]
+words = [word.strip() for word in open('/usr/share/dict/words').readlines()]
 
 for ind in range(count):
     amount = random.uniform(1.0, 1000)
