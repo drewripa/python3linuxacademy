387a8cc2e57a77dda4637d8cf59537bdcaa0bf2a
drewripa <ripa.andrew@gmail.com>
2018-03-19 16:34:46 +0200

    Added process kill

--- task_1_4.py
+++ task_1_4.py
@@ -9,7 +9,12 @@ args = parser.parse_args()
 #I'm using CentOS7 + httpd instead of Python simplehttpserver
 port = 'http' if args.port_number == 80 else args.port_number
 
-proc = subprocess.run(["sudo","lsof", "-n", f"-i6TCP:{port}"], stdout=subprocess.PIPE)
+text = (subprocess.run(["sudo","lsof", "-n", f"-i6TCP:{port}"], stdout=subprocess.PIPE)).stdout
 
-print(proc.stdout.decode())
+for line in text.splitlines():
+    line_args = line.split()
+    if line_args[2] == 'root':
+        print(f"Killing {line_args[1]} PID")
+        subprocess.run(["sudo", "kill", f"{line_args[1]}"])
+        break
 
