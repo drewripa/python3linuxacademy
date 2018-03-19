51149fb7b0387a2dcf918a78ff930f077d955073
drewripa <ripa.andrew@gmail.com>
2018-03-19 15:05:45 +0200

    Add 8th task

---
+++ task_1_4.py
@@ -0,0 +1,13 @@
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(description='Process killer aka Hitmen')
+parser.add_argument('port_number', help='Port number for assassination')
+
+args = parser.parse_args()
+
+#I'm using CentOS7 + httpd instead of Python simplehttpserver
+port = 'http' if args.port_number == 80 else args.port_number
+
+subprocess.run(["lsof", "-n", f"-i6TCP:{port}"])
+
